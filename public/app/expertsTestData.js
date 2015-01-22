7f8daf3aec24a2c630fa7e7c7a3f3945c87ce729
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-01-22 14:52:20 -0800

    using test data generated the experts list on main page

---
+++ public/app/expertsTestData.js
@@ -0,0 +1,27 @@
+var wayne = {
+  'name': 'Wayne',
+  'address': '1234 main lfwkjeoiwfe',
+  'profile': 'this is the profile of this awesome experts',
+  'ebayLink': 'ebayLink of Wayne',
+  'itemSold': 98347
+};
+var jason = {
+  'name': 'Jason',
+  'address': '1234 main wfweoijwoije',
+  'profile': 'this is the profile of this awesome experts',
+  'ebayLink': 'ebayLink of Jason',
+  'itemSold': 98347
+};
+var vijie = {
+  'name': 'Vijie',
+  'address': '1234 main fwlnhfwoehi',
+  'profile': 'this is the profile of this awesome experts',
+  'ebayLink': 'ebayLink of Vijie',
+  'itemSold': 98347
+};
+
+
+
+
+
+var experts = [wayne, vijie, jason];
--- public/app/fleabay.js
+++ public/app/fleabay.js
@@ -3,4 +3,6 @@ var fleabay = angular.module('fleabay', []);
 fleabay.controller('mainController', function($scope){
 	$scope.signUp = toggleSignUpPage;
 	$scope.logIn = toggleLogInPage;
+
+  $scope.expertList = experts;
 })
