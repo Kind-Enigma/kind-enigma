662ce468440f3612a8e17a7f6e65b3c7104b2dab
Jason Yang <jasonyang@Jasons-MacBook-Air.local>
2015-01-27 14:33:44 -0800

    Able to log in/log out

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,18 +1,72 @@
 var fleabayControllers = angular.module('fleabayControllers', []);
+var currentUser = {};
 
-fleabayControllers.controller('mainController', function($scope){
+fleabayControllers.controller('mainController', function($scope, $location, $http){
   $scope.signUp = toggleSignUpPage;
   $scope.logIn = toggleLogInPage;
+
+  $scope.realLogIn = function(){
+    var userCellNumber = $scope.userCellNumber;
+    var userPassword = $scope.userPassword;
+
+    $http.get('http://localhost:8080/api/user?CellNumber=' + userCellNumber).
+      success(function(data, status, headers, config){
+        $scope.user = data[0];
+        console.log(data[0]);
+        console.log(userPassword);
+        if (data[0].Password == userPassword){
+          console.log('Password is correct')
+          currentUser = data;
+          $location.path("/user")
+        }
+      }).
+      error(function(data, status, headers, config){
+        console.log('there is an error')
+      });
+  }
+
 
   $scope.expertList = experts;
   $scope.itemList = items;
 });
 
-fleabayControllers.controller('userController', function($scope){
-  $scope.user = testUser;
-  $scope.itemList = items;
-  $scope.postItem = userPostItem;
-  $scope.logOut = userLogOut;
+fleabayControllers.controller('userController', function($scope, $http, $location){
+  $scope.user = currentUser;
+  // $scope.itemList = items;
+  // $scope.postItem = userPostItem;
+  $scope.postItem = function(){
+    var title = $('#postItemTitle').val()
+    var description = $('#postItemDescription').val()
+    var price = $('#postItemPrice').val()
+    $http.post('http://localhost:8080/api/items',{
+      Owner: $scope.user.CellNumber,
+      Title: title,
+      Description: description,
+      // Address: $scope.user.Address,
+      // City: $scope.user.City,
+      // State: $scope.user.State,
+      // ZipCode: $scope.user.ZipCode,
+      // Images: [],
+      ListPrice: price,
+      // ListPercentage: 10,
+      // Expert: 'expert',
+      // ListDate: Date,
+      // AcceptedDate: Date
+
+    }). // database not uploaded yet
+    success(function(data, status, headers, confit){
+
+    }).
+    error(function(data, status, headers, confit){
+
+    });
+  };
+  $scope.logOut = function(){
+    currentUser = {};
+    $location.path('/')
+  }
+
+  // $scope.logOut = userLogOut;
 });
 
 fleabayControllers.controller('expertController', function($scope){
