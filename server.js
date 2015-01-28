b6d718c0fc762a856f045c5a0ad1315defb7e3cb
Wayne Muller <github@wainage.com>
2015-01-28 15:45:52 -0800

    Switched CORS to npm cors package

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var bodyParser = require('body-parser');
 var apiRouter = require('./router/apiRouter');
 var setupRouter = require('./router/setupRouter');
@@ -7,21 +8,14 @@ var app = express();
 var port = process.env.PORT || 1337;
 
 
-//CORS middleware
-var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://karmatest1.azurewebsites.net/');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  next();
-};
 
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public" ));
-app.use(allowCrossDomain);
+app.use(cors());
 
 app.use('/api', apiRouter);
 app.use('/api/setup', setupRouter);
