12b669de54aa13246d8b72af41b3b77b9defd286
Wayne Muller <github@wainage.com>
2015-02-02 16:53:41 -0800

    Fixed signup

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -152,6 +152,7 @@ fleabayControllers.controller('signUpController', function($scope, $http){
       }).
         success(function(data, status, headers, config){
           currentUser = data;
+          sessionStorage.user = JSON.stringify(data);
           $location.path('/expert')
         }).
         error(function(data, status, headers, config){
@@ -164,6 +165,7 @@ fleabayControllers.controller('signUpController', function($scope, $http){
       }).
         success(function(data, status, headers, config){
           currentUser = data;
+          sessionStorage.user = JSON.stringify(data);
           $location.path('/user')
         }).
         error(function(data, status, headers, config){
